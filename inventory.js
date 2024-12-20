6d605350955559ab10a69ce592c279157a74ab4b
Hatim <sakazokihatim@gmail.com>
2024-12-20 14:02:07 +0100

    ajout class inventory

--- inventory.js
+++ inventory.js
@@ -3,3 +3,8 @@ const fs = require('fs');
 const prompt = require('prompt-sync')(); // Initialize prompt-sync
 
 const DATA_FILE = 'inventaire.json';
+class Inventory {
+    constructor() {
+        this.products = [];
+        this.loadProducts(); 
+    }
